1ca3cb5ba4e1aa3e349679509f2606be0ba6bf63
Harikrishnan112 <harikrishnan33211@gmail.com>
2026-06-21 18:41:00 +0530

    Add mapping.js for encoding human-readable values

---
+++ mapping.js
@@ -0,0 +1,50 @@
+// Human-readable values → encoded values used in training
+
+const encodings = {
+    Gender: {
+        "Female": 0,
+        "Male": 1
+    },
+
+    FAVC: {
+        "No": 0,
+        "Yes": 1
+    },
+
+    SCC: {
+        "No": 0,
+        "Yes": 1
+    },
+
+    SMOKE: {
+        "No": 0,
+        "Yes": 1
+    },
+
+    familyhistorywithoverweight: {
+        "No": 0,
+        "Yes": 1
+    },
+
+    CALC: {
+        "No": 0,
+        "Sometimes": 1,
+        "Frequently": 2,
+        "Always": 3
+    },
+
+    CAEC: {
+        "No": 0,
+        "Sometimes": 1,
+        "Frequently": 2,
+        "Always": 3
+    },
+
+    MTRANS: {
+        "Automobile": 0,
+        "Motorbike": 1,
+        "Bike": 2,
+        "Public Transportation": 3,
+        "Walking": 4
+    }
+};
